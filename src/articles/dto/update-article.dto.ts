c55f907ed6932d4b6873a2ad3f0aabc3d8dc15d3
森 真輝人 <mkt.it.engr@gmail.com>
2022-11-08 15:50:21 +0900

    CRUD処理追加

--- src/articles/dto/update-article.dto.ts
+++ src/articles/dto/update-article.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateArticleDto } from './create-article.dto';
 
+//PartialTypeは引数の型を全てオプショナルにする
 export class UpdateArticleDto extends PartialType(CreateArticleDto) {}
