53be45f73afda255c4a16077f13e283ac43682ec
森 真輝人 <mkt.it.engr@gmail.com>
2022-11-08 18:17:53 +0900

    デコレータを使ってバリデーションする

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -5,6 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // validationのパイプをグローバルに使う（whitelist:trueでDTOにcreateを実行した時にDTOに書かれているプロパティ以外は削除する）
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  //Swagger作成
   const config = new DocumentBuilder()
     .setTitle('Median')
     .setDescription('The Median API description')
@@ -13,6 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  //アプリ起動
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
